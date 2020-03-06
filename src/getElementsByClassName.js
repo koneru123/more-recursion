e230ce7fd15cd5c665eac9ca3af507469a0631da
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-03-05 17:23:13 -0800

    getElementByClassName & stringifyJSON functions

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,33 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-};
+
+let getElementsByClassName = function (className) {
+
+  //output array
+  let newArr = [];
+  //create a inner closure function
+  let doesClassNameExist = (test) => {
+      // check if there are any classes and if the classNames contain the function argument
+      if (test.classList && (test.classList.value.indexOf(className) > -1)) {
+        // push, maybe here ???
+        newArr.push(test);
+        //console.log(newArr);
+      }
+      // check if there are any sub-nodes/children
+      if(test.childNodes) {
+        // childNodes is an array, loop through the childNodes
+        //for(let i = 0; i < test.childNodes.length; i++) {
+          // recursive function is called on each individual node element
+          //doesClassNameExist(test.childNodes[i]);
+        //}
+        test.childNodes.forEach(element => doesClassNameExist(element));
+      }
+  }
+  //function is invoked here for the first time
+  doesClassNameExist(document.body);
+  // return the result array
+  return newArr;
+}
+
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,73 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
+
+var stringifyJSON = function (obj) {
   // your code goes here
+  if(obj === null) {
+    return 'null';
+  }
+  if(typeof obj === 'boolean') {
+    return `${obj}`;
+  }
+  if (typeof obj === 'string') return `"${obj}"`;
+  if (typeof obj === 'number') return `${obj}`;
+  if (Array.isArray(obj)) {
+    if(obj.length === 0) {
+      return `[]`;
+    }
+    let output = [];
+    for (let i = 0; i < obj.length; i++) {
+      let current = obj[i];
+      if (current.length > 1) {
+        output.push(stringifyJSON(current));
+      } else {
+        output.push(current); //maybe add current.toString()
+      }
+    }
+    return output;
+  }
+  if (typeof obj === 'object') {
+    let emptyObjArr = [];
+    for (let [key, value] of Object.entries(obj)) {
+      emptyObjArr.push(`${stringifyJSON(key)}:${stringifyJSON(value)}`);
+    }
+    let emptyString = emptyObjArr.join(',')
+    return '{' + emptyString + '}';
+  }
 };
+
+/* let obj1 = {
+  name: 'zachary',
+  school: 'galvanize',
+  lives: 'california',
+};
+
+let obj2 = {
+  name: 'zachary',
+  school: "galvanize",
+  lives: 'california',
+  friends: {
+    girls: 4,
+    boys: 3
+  }
+}; */
+
+/* console.log(stringifyJSON('zach likes the color blue')); // ""hello""
+console.log(stringifyJSON(334))// "3"
+console.log(stringifyJSON([1, 2, [3, 4], [5, 6, 7]])) //"[1,2,3]"
+console.log(stringifyJSON([{ x: 5 }, { x: 6 }, { x: 7 }]))
+console.log(stringifyJSON({ x: 5 }))
+console.log(stringifyJSON(obj1))
+console.log(stringifyJSON(obj2))
+
+console.log('------------------------------ test break')
+
+console.log('stringify test-----', JSON.stringify('zach likes the color blue'));
+console.log('stringify test-----', JSON.stringify(334));
+console.log('stringify test-----', JSON.stringify([1, 2, [3, 4], [5, 6, 7]]));
+console.log('stringify test-----', JSON.stringify([{ x: 5 }, { x: 6 }, { x: 7 }]));
+console.log('stringify test-----', JSON.stringify({ x: 5 })); //'{"x":5}'
+console.log('stringify test-----', JSON.stringify(obj1));
+console.log('stringify test-----', JSON.stringify(obj2)); //'{"x":5}'
+ */
